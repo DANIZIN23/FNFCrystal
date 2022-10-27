bbba31cc9d780fc464e872e6e3b4350ddd1e80e1
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-27 00:23:06 -0300

    Update PlayState.hx

--- source/meta/state/PlayState.hx
+++ source/meta/state/PlayState.hx
@@ -143,7 +143,7 @@ class PlayState extends MusicBeatState
 	// private var dadStrums:Strumline;
 	// private var boyfriendStrums:Strumline;
 
-	public static var strumLines:FlxTypedGroup<Strumline>;
+	// public static var strumLines:FlxTypedGroup<Strumline>;
 	public static var strumHUD:Array<FlxCamera> = [];
 
 	private var allUIs:Array<FlxCamera> = [];
